72e819eafa1047fab6b6f22476998421fa7cf505
gnsyms <81622413+gnsyms@users.noreply.github.com>
2021-03-30 17:33:52 +0530

    Update PStest.ps1

--- PStest.ps1
+++ PStest.ps1
@@ -1,3 +1,4 @@
-Install-Module -Name PnP.PowerShell
+Install-PackageProvider Nuget -ForceBootstrap -Force
+Install-Module -Name SharePointPnPPowerShellOnline -Force -Verbose -Scope CurrentUser
 Write-host "Running PowerShell"
 Connect-PnPOnline -Url "https://svly.sharepoint.com/sites/gnsyms" -UseWebLogin
