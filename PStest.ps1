1b1156e9fc24823faa49793c081b9b5791ed0905
gnsyms <81622413+gnsyms@users.noreply.github.com>
2021-03-31 13:57:46 +0530

    Update PStest.ps1

--- PStest.ps1
+++ PStest.ps1
@@ -3,6 +3,8 @@ Write-host "Running PowerShell"
 $username = ${env:USERNAME}
 $password = ${env:PASSWORD}
 
+Write-host $username
+Write-host $password
 $encpassword = convertto-securestring -String $password -AsPlainText -Force
 $cred = new-object -typename System.Management.Automation.PSCredential -argumentlist $username, $password
 Connect-PnPOnline -Url "https://svly.sharepoint.com/sites/gnsyms" -Credentials $cred
